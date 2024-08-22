e68eec52f4239c697d79e21afa06e71b22aa5e4e
NickSdot <catch.github.com.nicksdot@nicksdot.dev>
2024-08-23 04:42:48 +0800

    feat(Routing): unused

--- tests/Integration/Routing/Enums.php
+++ tests/Integration/Routing/Enums.php
@@ -6,14 +6,3 @@ enum RouteNameEnum: string
 {
     case UserIndex = 'users.index';
 }
-
-enum RouteDomainEnum: string
-{
-    case DashboardDomain = 'dashboard.myapp.com';
-}
-
-enum IntegerEnum: int
-{
-    case One = 1;
-    case Two = 2;
-}
